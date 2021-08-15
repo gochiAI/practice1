446893f81c2facd878f17acaffccfbe210be5c06
gochi_AI <yumemi1204@protonmail.com>
2021-08-15 03:47:08 +0000

    finish

--- Main.py
+++ Main.py
@@ -2,5 +2,6 @@ S = open('/workspace/practice1/out/o.txt',mode='a')
 
 
 for Story in range(1,44):
+  Story = "{0:02d}".format(Story)
   Y=f'![](https://gitlab.com/gochiusa/DB/Official/Books/main-story/volume1/-/raw/main/Story/{Story}.jpeg)'
   S.write(Y+"\r\n")
