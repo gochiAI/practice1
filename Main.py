2e3c10b11c800677993619b4a065734b0482436c
gochi_AI <yumemi1204@protonmail.com>
2021-08-18 04:44:04 +0000

    j

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-S = open('/workspace/practice1/out/o.txt',mode='a')
+S = open('/workspace/practice1/out/i.txt',mode='a')
 
 
-for Story in range(1,44):
+for Story in range(1,52):
   Story = "{0:02d}".format(Story)
-  Y=f'![](https://gitlab.com/gochiusa/DB/Official/Books/main-story/volume1/-/raw/main/Story/{Story}.jpeg)'
+  Y=f'![](https://gitlab.com/gochiusa/DB/Official/Books/main-story/volume1/-/raw/main/Story/{Story}.png)'
   S.write(Y+"\r\n")
